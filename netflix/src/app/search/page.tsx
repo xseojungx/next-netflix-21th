bd789a210d20ae23436f29a39d71ceb1b12d7eef
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-07 19:51:46 +0900

    chore: overflow hidden 삭제

--- netflix/src/app/search/page.tsx
+++ netflix/src/app/search/page.tsx
@@ -2,11 +2,20 @@ import SearchInput from "@/components/search/SearchInput";
 import MovieCard from "@/components/search/MovieCard";
 const SearchPage = () => {
   return (
-    <div className="flex h-screen w-full flex-col overflow-hidden bg-black pt-11">
+    <div className="flex h-screen w-full flex-col bg-black pt-11">
       <SearchInput />
       <section className="mb-12 flex min-h-10 flex-col pt-5">
         <p className="h1 ml-2 text-white">Top Searches</p>
         <article className="mt-5 flex h-fit w-full flex-1 flex-col space-y-[0.19rem] overflow-y-auto">
+          <MovieCard /> <MovieCard />
+          <MovieCard /> <MovieCard />
+          <MovieCard /> <MovieCard />
+          <MovieCard /> <MovieCard />
+          <MovieCard /> <MovieCard />
+          <MovieCard /> <MovieCard />
+          <MovieCard /> <MovieCard />
+          <MovieCard /> <MovieCard />
+          <MovieCard /> <MovieCard />
           <MovieCard /> <MovieCard />
         </article>
       </section>

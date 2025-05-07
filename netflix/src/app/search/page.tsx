142d6d1a3503916d9951733b74902fa720579c68
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-07 14:08:49 +0900

    feat: search input

---
+++ netflix/src/app/search/page.tsx
@@ -0,0 +1,9 @@
+import SearchInput from "@/components/search/SearchInput";
+const SearchPage = () => {
+  return (
+    <div className="flex h-full w-full flex-col bg-black pt-11">
+      <SearchInput />
+    </div>
+  );
+};
+export default SearchPage;

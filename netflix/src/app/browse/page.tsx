a15fad6736ca467dad5990a9c673218353d4efcb
seoyeon5117 <seoyun020207@gmail.com>
2025-05-07 16:25:28 +0900

    feat: add play button

--- netflix/src/app/browse/page.tsx
+++ netflix/src/app/browse/page.tsx
@@ -1,4 +1,7 @@
 import NetflixIcon from "@/assets/images/NetflixIcon.svg";
+import PlusIcon from "@/assets/images/PlusIcon.svg";
+import PlayIcon from "@/assets/images/PlayIcon.svg";
+import InfoIcon from "@/assets/images/InfoIcon.svg";
 import Top10 from "@/assets/images/Top10.svg";
 
 const Home = () => {
@@ -40,6 +43,31 @@ const Home = () => {
             #2 in Nigeria Today
           </h1>
         </div>
+      </div>
+      <div className="flex w-full justify-between px-12 py-4">
+        <button
+          type="button"
+          className="flex w-12 cursor-pointer flex-col items-center justify-between"
+        >
+          <PlusIcon />
+          <span className="text-[.875rem] leading-[1.25rem]">My List</span>
+        </button>
+        <button
+          type="button"
+          className="flex shrink-0 cursor-pointer items-center gap-[1.125rem] rounded-[.375rem] bg-[#C3C4C4] py-2 pr-[.625rem] pl-5 hover:bg-[#8a8a8a]"
+        >
+          <PlayIcon />
+          <span className="text-[1.25rem] leading-[1.875rem] font-semibold text-black">
+            Play
+          </span>
+        </button>
+        <button
+          type="button"
+          className="flex w-12 cursor-pointer flex-col items-center justify-between"
+        >
+          <InfoIcon />
+          <span className="text-[.875rem] leading-[1.25rem]">Info</span>
+        </button>
       </div>
     </div>
   );

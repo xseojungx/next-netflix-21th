b7566a5c43cd195eb7c2bd810ab0ecd3d64096eb
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-07 19:40:16 +0900

    fix: px to rem

--- netflix/src/app/search/page.tsx
+++ netflix/src/app/search/page.tsx
@@ -6,8 +6,8 @@ const SearchPage = () => {
       <SearchInput />
       <section className="mb-12 flex min-h-10 flex-col pt-5">
         <p className="h1 ml-2 text-white">Top Searches</p>
-        <article className="mt-5 flex h-fit w-full flex-1 flex-col space-y-[3px] overflow-y-auto">
-          <MovieCard />
+        <article className="mt-5 flex h-fit w-full flex-1 flex-col space-y-[0.19rem] overflow-y-auto">
+          <MovieCard /> <MovieCard />
         </article>
       </section>
     </div>

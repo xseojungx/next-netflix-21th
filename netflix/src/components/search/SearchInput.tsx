c496062347934f7293488627357e678af9139718
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-10 23:00:22 +0900

    fix: clear search

--- netflix/src/components/search/SearchInput.tsx
+++ netflix/src/components/search/SearchInput.tsx
@@ -1,30 +1,33 @@
 import SearchIcon from "@/assets/images/SearchIcon.svg";
 import ExitIcon from "@/assets/images/ExitIcon.svg";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const SearchInput = () => {
   const router = useRouter();
-
+  const [searchQuery, setSearchQuery] = useState("");
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchQuery = e.target.value;
+    setSearchQuery(e.target.value);
     router.push(`/search?q=${encodeURIComponent(searchQuery)}`);
   };
 
   const handleClear = () => {
-    router.push('/search');
+    setSearchQuery("");
+    router.push("/search");
   };
 
   return (
     <div className="flex h-13 w-full max-w-md items-center gap-1 bg-[#424242] px-5 py-2">
-      <SearchIcon className="h-5 w-5 cursor-pointer" />
+      <SearchIcon className="h-5 w-5 flex-shrink-0 cursor-pointer" />
       <input
         type="text"
         placeholder="Search for a show, movie, genre, e.t.c."
-        className="flex-1 bg-transparent text-white placeholder-zinc-500 focus:outline-none"
+        className="flex-1 overflow-hidden bg-transparent text-white placeholder-zinc-500 focus:outline-none"
         onChange={handleSearch}
+        value={searchQuery}
       />
-      <ExitIcon 
-        className="cursor-pointer" 
+      <ExitIcon
+        className="h-5 w-5 flex-shrink-0 cursor-pointer"
         onClick={handleClear}
       />
     </div>

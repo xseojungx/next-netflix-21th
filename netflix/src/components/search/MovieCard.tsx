d27ca9e7c95650626ac7e01ddad90b2fede475a2
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-17 22:00:51 +0900

    fix: search input icon  flex-shrink-0

--- netflix/src/components/search/MovieCard.tsx
+++ netflix/src/components/search/MovieCard.tsx
@@ -20,18 +20,18 @@ const MovieCard = ({ movie }: MovieCardProps) => {
 
   return (
     <div
-      className="box-border flex h-19 w-full cursor-pointer items-center border-0 bg-[#424242] pr-3"
+      className="box-border flex h-[4.75rem] w-full cursor-pointer items-center border-0 bg-[#424242] pr-3"
       onClick={onCardClick}
     >
       <Image
-        style={{ objectFit: "cover", height: "4.75rem", width: "146px" }}
+        style={{ objectFit: "cover", height: "4.75rem", width: "9.125rem" }}
         src={imageUrl}
         alt={movie.title}
         height={76}
         width={146}
         loading="lazy"
       />
-      <span className="b1 ml-2 line-clamp-2 flex-1 leading-[1.4] text-white">
+      <span className="b1 ml-2 line-clamp-2 flex-1 leading-1.5 text-white">
         {movie.title}
       </span>
       <button type="button" className="cursor-not-allowed">

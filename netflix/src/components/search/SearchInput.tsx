d843111ec7191e2c5d9bcc4eb8b5474cc3a499c6
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-17 22:34:59 +0900

    fix: search input 줄어드는 문제 해결

--- netflix/src/components/search/SearchInput.tsx
+++ netflix/src/components/search/SearchInput.tsx
@@ -30,7 +30,7 @@ const SearchInput = () => {
   }, [debouncedQuery, router]);
 
   return (
-    <div className="flex h-13 w-full max-w-md items-center gap-1 bg-[#424242] px-5 py-2">
+    <div className="flex h-13 w-full max-w-md shrink-0 items-center gap-1 bg-[#424242] px-5 py-2">
       <SearchIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
       <input
         type="text"
